949c4053815fc6bf4be7a9287fe2141c25a8ed04
Ruthvik Jonna <ruthvikjonna@gmail.com>
2025-06-25 23:11:15 -0500

    added get_db dependency for session management

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -16,3 +16,10 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Create base
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
