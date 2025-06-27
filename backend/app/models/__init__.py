d33889bbe80f483e39181ab148a823ef60d747ed
Ruthvik Jonna <ruthvikjonna@gmail.com>
2025-06-27 18:04:07 -0500

    registered new intervention, climategridcell, and operator models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,1 +1,6 @@
 from .intervention import Intervention
+from .climate_grid_cell import ClimateGridCell
+from .operator import Operator
+
+# Import all models here for Alembic to detect them
+__all__ = ["Intervention", "ClimateGridCell", "Operator"]
