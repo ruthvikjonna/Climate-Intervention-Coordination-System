ad8fbac2eec711afbdb58690f3d5b4733a5524e9
Ruthvik Jonna <ruthvikjonna@gmail.com>
2025-06-27 23:43:31 -0500

    exposed new models for alembic and app registration

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,18 @@
 from .intervention import Intervention
 from .climate_grid_cell import ClimateGridCell
 from .operator import Operator
+from .satellite_data import SatelliteData
+from .intervention_impact import InterventionImpact
+from .optimization_result import OptimizationResult
+from .data_source import DataSource
 
 # Import all models here for Alembic to detect them
-__all__ = ["Intervention", "ClimateGridCell", "Operator"]
+__all__ = [
+    "Intervention", 
+    "ClimateGridCell", 
+    "Operator", 
+    "SatelliteData", 
+    "InterventionImpact", 
+    "OptimizationResult", 
+    "DataSource"
+]
